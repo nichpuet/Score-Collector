0154797f84fe0e5f0e21a3b6171ca9ec36dbb037
Nick Puetz <Napez702@hotmail.com>
2020-07-30 21:50:31 -0400

    Updates to the prepLol method

--- ScoreCollect.py
+++ ScoreCollect.py
@@ -1,12 +1,11 @@
 
-from twilio.rest import Client
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 hockeyKey = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=9"
 pandakey = "rjA_pWn2b0NohfOPbAfFcNamnLkWimENmiZ4_m8z48FQBbiKKKo"
-favsLol = ['Gen.G', 'Griffin']
+favsLol = ['Team Liquid', 'Team SoloMid', 'Top Esports', 'T1', 'DRX', 'G2 Esports', 'SK Gaming', 'Fnatic']
 #Add favs for counter strike
 #Add favs for Overwatch League
 
@@ -17,7 +16,7 @@ def jPrint(obj):
 def prepHock():
     response = requests.get(hockeyKey)
     r = response.json()
-    #jPrint(r)
+    jPrint(r)
     awayName = r['dates'][0]['games'][0]['teams']['away']['team']['name']
     awayScore = r['dates'][0]['games'][0]['teams']['away']['score']
     awayWins = r['dates'][0]['games'][0]['teams']['away']['leagueRecord']['wins']
@@ -34,57 +33,74 @@ def prepHock():
         if (int(current) >= 22 or int(current) <= 2):
             gameSend = homeName[0:3].upper() + " ("+str(homeScore)+") vs ("+str(awayScore)+") "+awayName[0:3].upper()
             records = str(homeWins)+ "/"+ str(homeLoss)+ "\t\t      "+str(awayWins)+ "/"+ str(awayLoss)
-            sendText(gameSend, records, "NHL\tFINAL")
+            sendable = "" + gameSend + "\n" + records
+            sendText(sendable)
     elif (gameState == "Live"):
         gameSend = homeName[0:3].upper() + " ("+str(homeScore)+") vs ("+str(awayScore)+") "+awayName[0:3].upper()
         records = str(homeWins)+ "/"+ str(homeLoss)+ "\t\t      "+str(awayWins)+ "/"+ str(awayLoss)
-        sendText(gameSend, records, "NHL\tLIVE")
+        sendable = "" + gameSend + "\n" + records
+        sendText(sendable)
 
 def prepLol():
-    response = requests.get("https://api.pandascore.co/lol/matches?token=rjA_pWn2b0NohfOPbAfFcNamnLkWimENmiZ4_m8z48FQBbiKKKo")
-    r = response.json()
-    for i in r:
-        league = i['league']['name']
-        if (league == 'KeSPA Cup'):
-            try:
-                teamA = i['opponents'][0]['opponent']['name']
-                teamB = i['opponents'][1]['opponent']['name']
-            except:
-                teamA = 'null'
-                teamB = 'null'
-            for k in favsLol:
-                if (teamA == k or teamB == k):
-                    gameTime = i['scheduled_at']
+    naString = "https://api.pandascore.co/leagues/league-of-legends-lcs/matches?token=rjA_pWn2b0NohfOPbAfFcNamnLkWimENmiZ4_m8z48FQBbiKKKo"
+    euString = "https://api.pandascore.co/leagues/league-of-legends-lec/matches?token=rjA_pWn2b0NohfOPbAfFcNamnLkWimENmiZ4_m8z48FQBbiKKKo"
+    krString = "https://api.pandascore.co/leagues/league-of-legends-lck-champions-korea/matches?token=rjA_pWn2b0NohfOPbAfFcNamnLkWimENmiZ4_m8z48FQBbiKKKo"
+    cnString = "https://api.pandascore.co/leagues/league-of-legends-lpl-china/matches?token=rjA_pWn2b0NohfOPbAfFcNamnLkWimENmiZ4_m8z48FQBbiKKKo"
 
-                    gameTimeHour = gameTime[11:16]
-                    gameTimeDate = gameTime[0:10].replace('-','/')
+    pandaString = [naString, euString, krString, cnString]
+    for i in pandaString:
+        response = requests.get(i)
+        r = response.json()
+        for i in r:
+            league = i['league']['name']
+            if (league == 'LPL' or league == 'LCS' or league == 'LCK'or league == "LEC"):
+                
+                try:
+                    teamA = i['opponents'][0]['opponent']['name']
+                    teamB = i['opponents'][1]['opponent']['name']
+                
+                except:
+                    teamA = 'null'
+                    teamB = 'null'
 
-                    temp = teamA + " vs " + teamB
-                    tempName = teamA + " vs " + teamB + " " + gameTimeDate + " " + gameTimeHour
+                #print(league, teamA, teamB)
+                formattedGames = []
+                for k in favsLol:
+                    
+                    if (teamA == k or teamB == k):
+                        gameTime = i['scheduled_at']
 
-                    print(tempName)
-                    #currently adds double of the last instant to list
+                        gameTimeHour = gameTime[11:16]
+                        gameTimeDate = gameTime[0:10]
+                        
+                        year = datetime.now().year
+                        month = datetime.now().month
+                        day = datetime.now().day
+                        currdate = '{}-{}-{}'.format(year, month, day)
+                        currdate = datetime.strptime(currdate, '%Y-%m-%d')
+                        gameTimeDateD = datetime.strptime(gameTimeDate, '%Y-%m-%d')
+
+                        delta = gameTimeDateD - currdate
+
+                        if delta.days < 10 and delta.days >= 0:
+                            matchName = teamA + " vs " + teamB + " " + gameTimeDate + " " + gameTimeHour
+                            if matchName in formattedGames:
+                                pass
+                            else: 
+                                formattedGames.append(matchName)
+                                print(matchName)
+                                #currently adds double of the last instant to list
 
 
-def sendText(gameSend, records, sport):
-    account_sid = 'ACfc01b14476a7a182b92c7c137b5d9e8d'
-    auth_token = '573dafe05f656858a64df5eeb6f026cc'
-    client = Client(account_sid, auth_token)
-    sendable = sport+"\n"+gameSend+ "\n"+ records
-    message = client.messages \
-                    .create(
-                         body=sendable ,
-                         from_='+13658000812',
-                         to='+15197033066'
-                     )
-
-    print(message.sid)
+def sendText(sendable):
+    #Should send the message to phone
+    x = sendable
 
 def main():
     #sendText()
     print()
-    prepHock()
-    #prepLol()
+    #prepHock()
+    prepLol()
     print()
 
 main()
